e91d2827a6eed8af8f5c0c23e80f2d95e2610884
Ileri Oyedele <ilerioyedele@gmail.com>
2017-07-10 01:52:47 -0700

    test_excel.php -> for testing purposes (this is a temp file)

---
+++ application/views/temp/test_excel.php
@@ -0,0 +1,18 @@
+<?php if (!isset($data)) { ?>
+
+<table border="1">
+  <tr>
+    <th>header 1</th>
+    <th>header 2</th>
+  </tr>
+  <tr>
+    <td>data 1</td>
+    <td>data 2</td>
+  </tr>
+  <tr>
+    <td>data 3</td>
+    <td>data 4</td>
+  </tr>
+</table>
+
+<?php } else {echo "data is set.";} ?>
