e7b70d5520d9078d607bbb33421b1474e3de27f9
Ileri Oyedele <ilerioyedele@gmail.com>
2017-07-10 01:50:23 -0700

    Started on the implementation of phpexcel.

--- application/controllers/Excel.php
+++ application/controllers/Excel.php
@@ -5,12 +5,45 @@ class Excel extends CI_Controller{
   public function __construct()
   {
     parent::__construct();
-    //Codeigniter : Write Less Do More
+    $this->load->library('excel');
   }
 
-  function index()
+  public function index()
   {
+    $file = './files/test.xlsx';
 
+    //load the excel library
+    $this->load->library('excel');
+
+    //read file from path
+    $objPHPExcel = PHPExcel_IOFactory::load($file);
+
+    //get only the Cell Collection
+    $cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
+
+    //extract to a PHP readable array format
+    foreach ($cell_collection as $cell) {
+        $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
+        $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
+        $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
+
+        //header will/should be in row 1 only. of course this can be modified to suit your need.
+        if ($row == 1) {
+            $header[$row][$column] = $data_value;
+        } else {
+            $arr_data[$row][$column] = $data_value;
+        }
+    }
+
+    //send the data in an array format
+    $data['header'] = $header;
+    $data['values'] = $arr_data;
+
+    $this->load->view('temp/test_excel', $data);
   }
 
+  public function loadtest()
+  {
+    $this->load->view('temp/test_excel');
+  }
 }
