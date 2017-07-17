33f045111a3c646d643c803115c7121378191f01
Ileri Oyedele <ilerioyedele@gmail.com>
2017-07-17 15:13:56 -0700

    finally managed to load all the data form an excel file. Next step getting it into the database.

--- application/controllers/Excel.php
+++ application/controllers/Excel.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Excel extends CI_Controller {
 
-  var $headers = [
+  var $_headers = [
     'subject',
     'course',
     'section',
@@ -21,45 +21,13 @@ class Excel extends CI_Controller {
     $this->load->library('excel/filters/myreadfilter');
   }
 
-  public function index()
+  public function test()
   {
-    $file = './files/test.xlsx';
-    $sheetName = 'Sheet2';
-
-    $objReader = PHPExcel_IOFactory::createReaderForFile($file);
-    $objReader->setLoadSheetsOnly($sheetName);
-    $objReader->setReadDataOnly(true);
-
-    //read file from path
-    $objPHPExcel = $objReader->load($file);
-
-    //get only the Cell Collection
-    $objWorksheet = $objPHPExcel->getActiveSheet();
-
-    $cell_collection = $objWorksheet->getCellCollection();
-    //extract to a PHP readable array format
-    foreach ($cell_collection as $cell) {
-      $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
-      $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
-      $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
-
-      //header will/should be in row 1 only. of course this can be modified to suit your need.
-      if ($row == 1) {
-        $header[$row][$column] = $data_value;
-      } else {
-        $arr_data[$row][$column] = $data_value;
-      }
-    }
-
-    $output = array('header'=>$header,'arr_data'=>$arr_data);
-
-    //send the data in an array format
-    $data['data'] = $output;
-
-    $this->load->view('temp/test_excel', $data);
+    $data['msg'] = time();
+    $this->load->view('temp/display', $data);
   }
 
-  public function ReadExcel($sheetName)
+  public function index($sheetName)
   {
     $sheetName = urldecode($sheetName);
 
@@ -68,16 +36,9 @@ class Excel extends CI_Controller {
       return;
     }
 
-    $filter = new MyReadFilter();
-    $filter->setColumn($this->headers);
-
     $objReader = PHPExcel_IOFactory::createReaderForFile($file);
     $objReader->setLoadSheetsOnly($sheetName);
-    $objReader->setReadDataOnly(true);
-    $objReader->setReadFilter($filter);
-
     $objPHPExcel = $objReader->load($file);
-
     $objWorksheet = $objPHPExcel->getActiveSheet();
 
     $cell_collection = $objWorksheet->getCellCollection();
@@ -85,18 +46,24 @@ class Excel extends CI_Controller {
       $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
       $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
       $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
-
       if ($row == 1) {
-        $header[$row][$column] = $data_value;
+        if (in_array(strtolower($data_value),$this->_headers)) {
+          $header[$row][$column] = $data_value;
+          $this->_headers[strtolower($data_value)] = $column;
+        }
       } else {
-        $arr_data[$row][$column] = $data_value;
+        if (in_array($column,$this->_headers)) {
+          if ($column == $this->_headers['start time'] || $column == $this->_headers['end time']) {
+            $arr_data[$row][$column] = PHPExcel_Style_NumberFormat::toFormattedString($objPHPExcel->getActiveSheet()->getCell($cell)->getCalculatedValue(), 'hh:mm:ss');
+          } else {
+            $arr_data[$row][$column] = $data_value;
+          }
+        }
       }
     }
 
     $output = array('header'=>$header,'arr_data'=>$arr_data);
-
     $data['data'] = $output;
-
     $this->load->view('temp/test_excel', $data);
   }
 }
