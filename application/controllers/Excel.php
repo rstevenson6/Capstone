ab4bdf5429d046234ef36d448b9e8a74b8ddb3ce
Ileri Oyedele <ilerioyedele@gmail.com>
2017-07-24 15:46:17 -0700

    Finished storing of data from excel into database.

--- application/controllers/Excel.php
+++ application/controllers/Excel.php
@@ -6,24 +6,20 @@ class Excel extends CI_Controller {
     'subject',
     'course',
     'section',
-    'termm',
+    'term',
     'primary act type',
     'days',
     'start time',
     'end time',
     'faculty name'
   ];
+  var $_headerCols = [];
 
   public function __construct()
   {
     parent::__construct();
+    $this->load->model('db_model');
     $this->load->library('excel/phpexcel');
-  }
-
-  public function test()
-  {
-    $data['msg'] = time();
-    $this->load->view('temp/display', $data);
   }
 
   public function index()
@@ -44,14 +40,16 @@ class Excel extends CI_Controller {
       $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
       $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
       $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
-      if ($row == 1) {
+      if ($row == 1) { // Row 1 contains headers
+        //If the header is in the predefined header list..
         if (in_array(strtolower($data_value),$this->_headers)) {
           $header[$row][$column] = $data_value;
-          $this->_headers[strtolower($data_value)] = $column;
+          $this->_headerCols[strtolower($data_value)] = $column;
         }
       } else {
-        if (in_array($column,$this->_headers)) {
-          if ($column == $this->_headers['start time'] || $column == $this->_headers['end time']) {
+        if (in_array($column,$this->_headerCols)) {
+          // Convert Excel time to readable time
+          if ($column == $this->_headerCols['start time'] || $column == $this->_headerCols['end time']) {
             $arr_data[$row][$column] = PHPExcel_Style_NumberFormat::toFormattedString($objPHPExcel->getActiveSheet()->getCell($cell)->getCalculatedValue(), 'hh:mm:ss');
           } else {
             $arr_data[$row][$column] = $data_value;
@@ -61,7 +59,71 @@ class Excel extends CI_Controller {
     }
 
     $output = array('header'=>$header,'arr_data'=>$arr_data);
+    echo var_dump($output);
+    $this->saveDataToDatabase($output);
     $data['data'] = $output;
-    $this->load->view('temp/test_excel', $data);
+  }
+
+  private function saveDataToDatabase($data)
+  {
+    $profs=[]; // to keep trach of seen profs
+    foreach ($data['arr_data'] as $items) {
+      $profName=$dept=$unit=null;
+      $profName = $items[$this->_headerCols['faculty name']];
+      $dept = $items[$this->_headerCols['subject']];
+      if (!in_array($profName,$profs) && isset($profName)) {
+        // save prof to database | this needs to happen first because of forein key constraints
+        $this->db_model->InsertProf($profName, $dept, $unit);
+        array_push($profs,$profName);
+      }
+    }
+    for ($i=2;$i<count($data['header'][1])+2;$i++) {
+      $subj=$courseNo=$section=$term=$actType=$days=$startTime=$endTime=$instructor=$TAName=null;
+      foreach ($this->_headerCols as $key => $value) {
+        switch ($key) {
+          case 'subject':
+            $subj = $data['arr_data'][$i][$value];
+            break;
+
+          case 'course':
+            $courseNo = $data['arr_data'][$i][$value];
+            break;
+
+          case 'section':
+            $section = $data['arr_data'][$i][$value];
+            break;
+
+          case 'term':
+            $term = $data['arr_data'][$i][$value];
+            break;
+
+          case 'primary act type':
+            $actType = $data['arr_data'][$i][$value];
+            break;
+
+          case 'days':
+            $days = $data['arr_data'][$i][$value];
+            break;
+
+          case 'start time':
+            $startTime = $data['arr_data'][$i][$value];
+            break;
+
+          case 'end time':
+            $endTime = $data['arr_data'][$i][$value];
+            break;
+
+          case 'faculty name':
+            $instructor = $data['arr_data'][$i][$value];
+            break;
+
+          default:
+            exit("Error: unknown key\n".debug_backtrace());
+            break;
+        }
+      }
+      // save class to database
+      $this->db_model->InsertClass($subj, $courseNo, $section, $term, $actType, $days, $startTime, $endTime, $instructor, $TAName);
+    }
   }
 }
